c9b097c7c879dafb4e7e7ef68e292e5dc439092f
pepepancho888 <pepepanchocepillo@gmail.com>
2022-11-10 12:25:43 -0500

    Add function to play a round of the game

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -8,11 +8,35 @@ function getRandomNumber(){
 function getComputerChoice() {
     let computerChoice = getRandomNumber();
     if (computerChoice === 1) {
-        return "Rock";
+        return "ROCK";
     } else if (computerChoice === 2) {
-        return "Paper";
+        return "PAPER";
     } else if (computerChoice === 3){
-        return "Scissors";
+        return "SCISSORS";
     }
 }
 
+//This function plays a round of the game and return the result
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    if (playerSelection === computerSelection){
+        return `It's a tie. ${playerSelection} equals ${computerSelection}`;
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        return "You win! ROCK beats SCISSORS";
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        return "You lose! PAPER beats ROCK";
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        return "You lose! SCISSORS beats PAPER";
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return "You Win! PAPER beats ROCK";
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        return "You lose! ROCK beats SCISSORS";
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        return "You Win! SCISSORS beats PAPER";
+    }
+}
+
+let playerSelection = "paPER";
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
