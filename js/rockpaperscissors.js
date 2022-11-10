b38a2a0976d2ebc12c20f5f0da9273929410d0b1
pepepancho888 <pepepanchocepillo@gmail.com>
2022-11-10 18:13:43 -0500

    Change game function to count the number of wins
    Also indicate the score and show the winner

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -22,29 +22,54 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
     if (playerSelection === computerSelection){
-        return `It's a tie. ${playerSelection} equals ${computerSelection}`;
+        console.log(`It's a tie. ${playerSelection} equals ${computerSelection}`);
+        return "Tie";
     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-        return "You win! ROCK beats SCISSORS";
+        console.log("You win! ROCK beats SCISSORS");
+        return "Player";
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        return "You lose! PAPER beats ROCK";
+        console.log("You lose! PAPER beats ROCK");
+        return "Computer";
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        return "You lose! SCISSORS beats PAPER";
+        console.log("You lose! SCISSORS beats PAPER");
+        return "Computer";
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-        return "You Win! PAPER beats ROCK";
+        console.log("You Win! PAPER beats ROCK");
+        return "Player";
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        return "You lose! ROCK beats SCISSORS";
+        console.log("You lose! ROCK beats SCISSORS");
+        return "Computer"
     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-        return "You Win! SCISSORS beats PAPER";
+        console.log("You Win! SCISSORS beats PAPER");
+        return "Player";
     }
 }
 
 // This function plays the game
 function game() {
-    for (let i = 0; i < 5; i++) {
+    let playerWins = 0;
+    let computerWins = 0;
+    while (playerWins !== 5 || computerWins !== 5) {
         let playerChoice = prompt("Choose Rock Paper Scissors");
         let computerChoice = getComputerChoice();
-        let roundResult = playRound(playerChoice, computerChoice);
-        console.log(roundResult);
+        let roundWinner = playRound(playerChoice, computerChoice);
+        if (roundWinner === "Player") {
+            playerWins += 1;
+            console.log(`Player: ${playerWins} Computer: ${computerWins}`);    
+        } else if (roundWinner === "Computer") {
+            computerWins += 1;
+            console.log(`Player: ${playerWins} Computer: ${computerWins}`);
+        } else if (roundWinner === "Tie") {
+            console.log(`Player: ${playerWins} Computer: ${computerWins}`);
+        }
+        if (playerWins === 5) {
+            console.log("Player Wins");
+            break;
+        }
+        if (computerWins === 5) {
+            console.log("Computer Wins");
+            break;
+        }
     }
 }
 
